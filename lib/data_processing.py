corrected ASD calculation for plotting

lib/data_processing.py
@@ -20,6 +20,27 @@ from stats import *
 import signals as s
 
 
+# ************************************************************************ #
+# This module contains the FileData class, used to extract data from a
+# single hdf5 file, and the AggregateData class, used to aggregate data
+# relevant to the physics analysis from many FileData objects. This code
+# builds on previous analysis code written by Chas Blakemore, Alex Rider,
+# David Moore, and others.
+
+# CONVENTIONS USED IN THIS MODULE:
+# Variables with "fft" in their name are the discrete Fourier transforms
+# of a given time series, scaled such that their magnitude at each frequency
+# gives the peak amplitude. For plotting, we prefer to show the amplitude
+# spectral density, calculated from the RMS amplitude spectrum by dividing
+# each point by the frequency bin width (equal to the sampling frequency
+# divided by the number of samples). With this definition, the square of
+# the amplitude spectral density is equal to the PSD as given by
+# scipy.signal.welch, up to the effects of overlap, windowing, and detrending.
+# 
+#                   -- Clarke Hardy, October 2023 --
+# ************************************************************************ #
+
+
 class FileData:
 
     def __init__(self,path=''):
@@ -55,9 +76,6 @@ class FileData:
         self.pspd_ffts = np.array(((),(),()))
         self.pspd_ffts_full = np.array(((),(),()))
         self.pspd_sb_ffts = np.array(((),(),()))
-        self.cross_asds = np.array(((),(),()))
-        self.cross_asds_full = np.array(((),(),()))
-        self.cross_sb_asds = np.array(((),(),()))
         self.template_ffts = np.array(())
         self.template_params = np.array(())
         self.cant_fft = np.array(())
@@ -118,36 +136,35 @@ class FileData:
         '''
         Reads raw data and metadata from an hdf5 file directly into a dict.
         '''
-        f = h5py.File(self.file_name, 'r')
         dd = {}
-        dd['cant_data'] = np.array(f['cant_data'],dtype=np.float64)
-        dd['quad_data'] = np.array(f['quad_data'],dtype=np.float64)
-        try:
-            dd['seismometer'] = np.array(f['seismometer'])
-            dd['laser_power'] = np.array(f['laser_power'])
-            dd['p_trans'] = np.array(f['p_trans'])
-            dd['pspd_data'] = np.array(f['PSPD'])
-        except:
-            dd['seismometer'] = np.zeros_like(dd['cant_data'][0])
-            dd['laser_power'] = np.zeros_like(dd['cant_data'])
-            dd['pspd_data'] = np.zeros_like(dd['cant_data'])
-            dd['p_trans'] = np.zeros_like(dd['cant_data'])
-        dd['timestamp_ns'] = os.stat(self.file_name).st_mtime*1e9
-        dd['fsamp'] = f.attrs['Fsamp']/f.attrs['downsamp']
-        try:
-            dd['cantilever_axis'] = f.attrs['cantilever_axis']
-            dd['cantilever_freq'] = f.attrs['cantilever_freq']
-            cant_voltages = list(f['cantilever_settings'])
-        except:
-            dd['cantilever_axis'] = 0
-            dd['cantilever_freq'] = 0
-            cant_voltages = np.zeros(6)
-        dd['cantilever_DC'] = [cant_voltages[i] for i in [0,2,4]]
-        dd['cantilever_amp'] = [cant_voltages[i] for i in [1,3,5]]
-        dd['bead_height'] = f.attrs['bead_height']
+        with h5py.File(self.file_name, 'r') as f:
+            dd['cant_data'] = np.array(f['cant_data'],dtype=np.float64)
+            dd['quad_data'] = np.array(f['quad_data'],dtype=np.float64)
+            try:
+                dd['seismometer'] = np.array(f['seismometer'])
+                dd['laser_power'] = np.array(f['laser_power'])
+                dd['p_trans'] = np.array(f['p_trans'])
+                dd['pspd_data'] = np.array(f['PSPD'])
+            except:
+                dd['seismometer'] = np.zeros_like(dd['cant_data'][0])
+                dd['laser_power'] = np.zeros_like(dd['cant_data'][0])
+                dd['pspd_data'] = np.zeros_like(dd['cant_data'])
+                dd['p_trans'] = np.zeros_like(dd['cant_data'][0])
+            dd['timestamp_ns'] = os.stat(self.file_name).st_mtime*1e9
+            dd['fsamp'] = f.attrs['Fsamp']/f.attrs['downsamp']
+            try:
+                dd['cantilever_axis'] = f.attrs['cantilever_axis']
+                dd['cantilever_freq'] = f.attrs['cantilever_freq']
+                cant_voltages = list(f['cantilever_settings'])
+            except:
+                dd['cantilever_axis'] = 0
+                dd['cantilever_freq'] = 0
+                cant_voltages = np.zeros(6)
+            dd['cantilever_DC'] = [cant_voltages[i] for i in [0,2,4]]
+            dd['cantilever_amp'] = [cant_voltages[i] for i in [1,3,5]]
+            dd['bead_height'] = f.attrs['bead_height']
 
         self.data_dict = dd
-        f.close()
 
     
     def get_laser_power(self):
@@ -395,22 +412,22 @@ class FileData:
         # transfer function data should be stored here in a folder named by the date
         if tf_path is None:
             tf_path = '/data/new_trap_processed/calibrations/transfer_funcs/'+str(self.date)+'/TF.h5'
-        tf_file = h5py.File(tf_path,'r')
 
-        ### Compute TF at frequencies of interest. Appropriately inverts
-        ### so we can map response -> drive
-        Harr = np.zeros((len(freqs), 3, 3), dtype=complex)
-        Harr[:,0,0] = 1/signal.freqs_zpk(tf_file['fits/'+sensor+'/zXX'], tf_file['fits/'+sensor+'/pXX'], \
-                                         tf_file['fits/'+sensor+'/kXX']/tf_file.attrs['scaleFactors_'+sensor][0], 2*np.pi*freqs)[1]
-        Harr[:,1,1] = 1/signal.freqs_zpk(tf_file['fits/'+sensor+'/zYY'], tf_file['fits/'+sensor+'/pYY'], \
-                                         tf_file['fits/'+sensor+'/kYY']/tf_file.attrs['scaleFactors_'+sensor][1], 2*np.pi*freqs)[1]
-        Harr[:,2,2] = 1/signal.freqs_zpk(tf_file['fits/'+sensor+'/zZZ'], tf_file['fits/'+sensor+'/pZZ'], \
-                                         tf_file['fits/'+sensor+'/kZZ']/tf_file.attrs['scaleFactors_'+sensor][2], 2*np.pi*freqs)[1]
-        tf_file.close()
+        with h5py.File(tf_path,'r') as tf_file:
+            ### Compute TF at frequencies of interest. Appropriately inverts
+            ### so we can map response -> drive
+            Harr = np.zeros((len(freqs), 3, 3), dtype=complex)
+            Harr[:,0,0] = 1/signal.freqs_zpk(tf_file['fits/'+sensor+'/zXX'], tf_file['fits/'+sensor+'/pXX'], \
+                                            tf_file['fits/'+sensor+'/kXX']/tf_file.attrs['scaleFactors_'+sensor][0], 2*np.pi*freqs)[1]
+            Harr[:,1,1] = 1/signal.freqs_zpk(tf_file['fits/'+sensor+'/zYY'], tf_file['fits/'+sensor+'/pYY'], \
+                                            tf_file['fits/'+sensor+'/kYY']/tf_file.attrs['scaleFactors_'+sensor][1], 2*np.pi*freqs)[1]
+            Harr[:,2,2] = 1/signal.freqs_zpk(tf_file['fits/'+sensor+'/zZZ'], tf_file['fits/'+sensor+'/pZZ'], \
+                                            tf_file['fits/'+sensor+'/kZZ']/tf_file.attrs['scaleFactors_'+sensor][2], 2*np.pi*freqs)[1]
+            tf_file.close()
 
-        # Only apply the TF to frequencies below some frequency
-        max_freq_ind = np.argmin( np.abs(freqs - max_freq) )
-        Harr[max_freq_ind+1:,:,:] = 0.0+0.0j
+            # Only apply the TF to frequencies below some frequency
+            max_freq_ind = np.argmin( np.abs(freqs - max_freq) )
+            Harr[max_freq_ind+1:,:,:] = 0.0+0.0j
 
         return Harr
     
@@ -595,11 +612,6 @@ class FileData:
         pspd_ffts = np.zeros((3, len(self.good_inds)), dtype=np.complex128)
         pspd_sb_ffts = np.zeros((3, len(self.good_inds)*noise_bins), dtype=np.complex128)
 
-        # initialize arrays for the pspd ffts of x, y, and z
-        cross_asds_full = np.zeros((3, len(self.freqs)), dtype=np.complex128)
-        cross_asds = np.zeros((3, len(self.good_inds)), dtype=np.complex128)
-        cross_sb_asds = np.zeros((3, len(self.good_inds)*noise_bins), dtype=np.complex128)
-
         # get the fft for the cantilever data along the driven axis
         cant_fft_full = np.fft.rfft(self.cant_pos_calibrated[self.drive_ind])
 
@@ -616,17 +628,12 @@ class FileData:
             # get the fft for the given axis and multiply by the calibration factor
             qpd_fft = np.fft.rfft(data_qpd[resp])
             pspd_fft = np.fft.rfft(data_pspd[resp])
-            _,cross_psd = signal.csd(data_pspd[resp],data_qpd[resp],window='boxcar',\
-                                     fs=self.fsamp,nperseg=self.nsamp)
-            cross_asd = np.sqrt(np.abs(cross_psd))*np.exp(1j*(np.angle(cross_psd)-np.angle(pspd_fft)))
 
             # add the fft to the existing array, which was initialized with zeros
             qpd_ffts_full[resp] += qpd_fft
             qpd_ffts[resp] += qpd_fft[self.good_inds]
             pspd_ffts_full[resp] += pspd_fft
             pspd_ffts[resp] += pspd_fft[self.good_inds]
-            cross_asds_full[resp] += cross_asd
-            cross_asds[resp] += cross_asd[self.good_inds]
 
             # now create a list of some number of indices on either side of the harmonic
             sideband_inds = []
@@ -652,7 +659,6 @@ class FileData:
             # finally, add the data at these indices to the array for this axis
             qpd_sb_ffts[resp] += qpd_fft[sideband_inds]
             pspd_sb_ffts[resp] += pspd_fft[sideband_inds]
-            cross_sb_asds[resp] += cross_asd[sideband_inds]
 
         # normalize the ffts
         norm_factor = 2./self.nsamp
@@ -669,12 +675,9 @@ class FileData:
         self.qpd_ffts = qpd_ffts
         self.pspd_ffts_full = pspd_ffts_full
         self.pspd_ffts = pspd_ffts
-        self.cross_asds_full = cross_asds_full
-        self.cross_asds = cross_asds
         self.cant_fft = cant_fft
         self.qpd_sb_ffts = qpd_sb_ffts
         self.pspd_sb_ffts = pspd_sb_ffts
-        self.cross_sb_asds = cross_sb_asds
 
 
     def make_templates(self,signal_model,p0_bead,cant_vec=None,num_harms=10):
@@ -895,7 +898,6 @@ class AggregateData:
         mean_cant_pos = []
         freqs = []
         good_inds = []
-        cant_inds = []
         fund_ind = []
         qpd_ffts = []
         qpd_ffts_full = []
@@ -903,9 +905,6 @@ class AggregateData:
         pspd_ffts = []
         pspd_ffts_full = []
         pspd_sb_ffts = []
-        cross_asds_full = []
-        cross_asds = []
-        cross_sb_asds = []
         template_ffts = []
         template_params = []
         cant_fft = []
@@ -925,7 +924,6 @@ class AggregateData:
             quad_raw_data.append(f.quad_raw_data)
             mean_cant_pos.append(f.mean_cant_pos)
             good_inds.append(f.good_inds)
-            cant_inds.append(f.cant_inds)
             fund_ind.append(f.fund_ind)
             freqs.append(f.freqs)
             qpd_ffts.append(f.qpd_ffts)
@@ -934,9 +932,6 @@ class AggregateData:
             pspd_ffts.append(f.pspd_ffts)
             pspd_ffts_full.append(f.pspd_ffts_full)
             pspd_sb_ffts.append(f.pspd_sb_ffts)
-            cross_asds.append(f.cross_asds)
-            cross_asds_full.append(f.cross_asds_full)
-            cross_sb_asds.append(f.cross_sb_asds)
             template_ffts.append(f.template_ffts)
             template_params.append(f.template_params)
             cant_fft.append(f.cant_fft)
@@ -950,13 +945,12 @@ class AggregateData:
         agg_dict['mean_laser_power'] = np.array(mean_laser_power)
         agg_dict['laser_power_full'] = np.array(laser_power_full)
         agg_dict['mean_p_trans'] = np.array(mean_p_trans)
-        agg_dict['p_trans_full'] = np.array([p_trans_full])
+        agg_dict['p_trans_full'] = np.array(p_trans_full)
         agg_dict['cant_raw_data'] = np.array(cant_raw_data)
         agg_dict['quad_raw_data'] = np.array(quad_raw_data)
         agg_dict['mean_cant_pos'] = np.array(mean_cant_pos)
         agg_dict['freqs'] = np.array(freqs)
         agg_dict['good_inds'] = np.array(good_inds)
-        agg_dict['cant_inds'] = np.array(cant_inds)
         agg_dict['fund_ind'] = np.array(fund_ind)
         agg_dict['qpd_ffts'] = np.array(qpd_ffts)
         agg_dict['qpd_ffts_full'] = np.array(qpd_ffts_full)
@@ -964,9 +958,6 @@ class AggregateData:
         agg_dict['pspd_ffts'] = np.array(pspd_ffts)
         agg_dict['pspd_ffts_full'] = np.array(pspd_ffts_full)
         agg_dict['pspd_sb_ffts'] = np.array(pspd_sb_ffts)
-        agg_dict['cross_asds'] = np.array(cross_asds)
-        agg_dict['cross_asds_full'] = np.array(cross_asds_full)
-        agg_dict['cross_sb_asds'] = np.array(cross_sb_asds)
         agg_dict['template_ffts'] = np.array(template_ffts)
         agg_dict['template_params'] = np.array(template_params)
         agg_dict['cant_fft'] = np.array(cant_fft)
@@ -987,7 +978,7 @@ class AggregateData:
         files are first loaded, but never by the user.
         '''
         # initialize the list of bin indices
-        self.bin_indices = np.zeros((len(self.file_list),8)).astype(np.int32)
+        self.bin_indices = np.zeros((len(self.file_list),9)).astype(np.int32)
 
         # first bin by diam_bead, p0_bead, and descrips, basically already done when the data was read in
         for i in range(len(self.num_files)):
@@ -1073,7 +1064,7 @@ class AggregateData:
         # p0_bead and diam_bead are done when data is loaded. Here, binning is done in
         # cantilever x, cantilever z, and bias. Each row of bin_indices is of the format
         # [diam_ind, p0_bead_ind, descrips, cant_x_ind, cant_z_ind, seismometer_ind, \
-        # bias_ind, is_bad]
+        # bias_ind, spec_ind, is_bad]
 
         # first create bins in x and z given the bin widths provided
         x_lower_edge = min(self.agg_dict['mean_cant_pos'][:,0]) - cant_bin_widths[0]/2.
@@ -1239,6 +1230,63 @@ class AggregateData:
             print('Warning: no data matching specified cuts!')
             
         return list(np.sort(slice_indices))
+    
+
+    def get_spectral_densities(self):
+        '''
+        Compute the amplitude spectral density for each axis and sensor, averaging over datasets
+        with the same conditions as defined by bin_indices.
+        '''
+
+        print('Computing RMS amplitude spectral density for each set of run conditions...')
+
+        # get all unique combinations of run conditions
+        index_arrays = np.unique(self.bin_indices,axis=0)
+
+        # get sampling parameters in order to compute ASD from the DFTs
+        nsamp = self.agg_dict['times'].shape[1]
+        freqs = self.agg_dict['freqs'][0]
+        fsamp = 2.*freqs[-1]
+        fft_to_asd = np.sqrt(nsamp/2./fsamp)
+
+        # initialize lists to store the spectra
+        qpd_asds = []
+        pspd_asds = []
+
+        # get a list of indices for each unique set of run conditions
+        for ind,index_array in enumerate(index_arrays):
+            unique_inds = np.where([np.all(self.bin_indices[i,:]==index_array) \
+                                      for i in range(self.bin_indices.shape[0])])[0]
+
+            # average the PSDs to get the final ASD
+            qpd_asd_x = np.sqrt(np.mean(np.abs(self.agg_dict['qpd_ffts_full'][unique_inds][:,0,:]*fft_to_asd)**2,axis=0))
+            qpd_asd_y = np.sqrt(np.mean(np.abs(self.agg_dict['qpd_ffts_full'][unique_inds][:,1,:]*fft_to_asd)**2,axis=0))
+            qpd_asd_z = np.sqrt(np.mean(np.abs(self.agg_dict['qpd_ffts_full'][unique_inds][:,2,:]*fft_to_asd)**2,axis=0))
+            pspd_asd_x = np.sqrt(np.mean(np.abs(self.agg_dict['pspd_ffts_full'][unique_inds][:,0,:]*fft_to_asd)**2,axis=0))
+            pspd_asd_y = np.sqrt(np.mean(np.abs(self.agg_dict['pspd_ffts_full'][unique_inds][:,1,:]*fft_to_asd)**2,axis=0))
+
+            qpd_asds.append(np.array((qpd_asd_x,qpd_asd_y,qpd_asd_z)))
+            pspd_asds.append(np.array((pspd_asd_x,pspd_asd_y)))
+
+            # update the bin indices
+            self.bin_indices[:,7][unique_inds] = ind
+
+        # add as class attributes
+        self.qpd_asds = np.array(qpd_asds)
+        self.pspd_asds = np.array(pspd_asds)
+
+        print('Amplitude spectral densities computed for {} sets of run conditions.'.format(self.qpd_asds.shape[0]))
+    
+
+    def drop_full_ffts(self):
+        '''
+        Drops the complete spectra for each file from the dictionary to save memory. To be called
+        after the data has been used for basic plotting.
+        '''
+
+        # keep the first two dimensions the same to avoid screwing up indexing
+        self.agg_dict['qpd_ffts_full'] = np.empty_like(self.agg_dict['qpd_ffts_full'][:,:,0])[...,np.newaxis]
+        self.agg_dict['pspd_ffts_full'] = np.empty_like(self.agg_dict['pspd_ffts_full'][:,:,0])[...,np.newaxis]
 
 
     def diagonalize_qpd(self,fit_inds=None,peak_guess=[400.,370.],width_guess=[10.,10.],plot=False):
@@ -1263,8 +1311,10 @@ class AggregateData:
             freq_inds.append(np.argmin(np.abs(freqs-f)))
 
         nsamp = self.agg_dict['quad_raw_data'][:,0,:].shape[1]
+        fsamp = 2.*freqs[-1]
+        fft_to_asd = np.sqrt(nsamp/2./fsamp)
 
-        # get mean of the ffts for the data used for the fit
+        # get RMS of the ffts for the data used for the fit
         mean_ffts_x = np.sqrt(np.mean(np.abs(np.fft.rfft(self.agg_dict['quad_raw_data'][fit_inds][:,0,:])*2./nsamp)**2,axis=0))
         mean_ffts_y = np.sqrt(np.mean(np.abs(np.fft.rfft(self.agg_dict['quad_raw_data'][fit_inds][:,1,:])*2./nsamp)**2,axis=0))
 
@@ -1309,10 +1359,10 @@ class AggregateData:
 
         if plot:
             fig,ax = plt.subplots(2,1,sharex=True)
-            ax[0].semilogy(freqs,np.abs(fft_qpd_1),label='Q1')
-            ax[0].semilogy(freqs,np.abs(fft_qpd_2),label='Q2')
-            ax[0].semilogy(freqs,np.abs(fft_qpd_3),label='Q3')
-            ax[0].semilogy(freqs,np.abs(fft_qpd_4),label='Q4')
+            ax[0].semilogy(freqs,np.abs(fft_qpd_1)*fft_to_asd,label='Q1')
+            ax[0].semilogy(freqs,np.abs(fft_qpd_2)*fft_to_asd,label='Q2')
+            ax[0].semilogy(freqs,np.abs(fft_qpd_3)*fft_to_asd,label='Q3')
+            ax[0].semilogy(freqs,np.abs(fft_qpd_4)*fft_to_asd,label='Q4')
             ax[1].plot(freqs,np.angle(fft_qpd_1)*180./np.pi,'.',ms='2',label='Q1')
             ax[1].plot(freqs,np.angle(fft_qpd_2)*180./np.pi,'.',ms='2',label='Q2')
             ax[1].plot(freqs,np.angle(fft_qpd_3)*180./np.pi,'.',ms='2',label='Q3')
@@ -1399,12 +1449,8 @@ class AggregateData:
 
         lambdas = self.agg_dict['template_params'][file_index]
         yuk_ffts = self.agg_dict['template_ffts'][file_index]
-        if sensor=='both':
-            bead_ffts = self.agg_dict['cross_asds'][file_index]
-            bead_sb_ffts = self.agg_dict['cross_sb_asds'][file_index]
-        else:
-            bead_ffts = self.agg_dict[sensor+'_ffts'][file_index]
-            bead_sb_ffts = self.agg_dict[sensor+'_sb_ffts'][file_index]
+        bead_ffts = self.agg_dict[sensor+'_ffts'][file_index]
+        bead_sb_ffts = self.agg_dict[sensor+'_sb_ffts'][file_index]
         num_sb = int(bead_sb_ffts.shape[1]/bead_ffts.shape[1])
         good_inds = self.agg_dict['good_inds'][file_index]
         sig_likes = self.agg_dict['sig_likes'][file_index][:,good_inds]
@@ -1774,7 +1820,3 @@ class AggregateData:
             self.signal_models = []
 
         print('Loaded AggregateData object from '+path)
-        
-
-
-                    

lib/plotting.py
@@ -7,6 +7,7 @@ from scipy import signal
 import h5py
 from funcs import *
 
+
 def polar_plots(agg_dict,indices=None,axis_ind=0,first_harm=2,sensor='qpd',\
                 amp_bins=np.logspace(-17,-15, 30),phase_bins=np.linspace(-np.pi,np.pi,60),\
                     vmax=50,plot_templates=True,alphas=np.array((1e8,1e9,1e10))):
@@ -148,6 +149,7 @@ def cross_coupling(agg_dict,qpd_diag_mat,p_x=None,p_y=None,plot_inds=None):
 
     freqs = agg_dict['freqs'][0]
     nsamp = raw_qpd_1.shape[1]
+    fsamp = 2.*freqs[-1]
 
     signal_mat = np.array((raw_qpd_1,raw_qpd_2,raw_qpd_3,raw_qpd_4))
 
@@ -164,17 +166,17 @@ def cross_coupling(agg_dict,qpd_diag_mat,p_x=None,p_y=None,plot_inds=None):
     x_corr = new_resp[0,:,:]
     y_corr = new_resp[1,:,:]
 
-    fft_x_raw = np.sqrt(np.mean(np.abs(np.fft.rfft(x_raw)*2./nsamp)**2,axis=0))
-    fft_y_raw = np.sqrt(np.mean(np.abs(np.fft.rfft(y_raw)*2./nsamp)**2,axis=0))
-    fft_x_corr = np.sqrt(np.mean(np.abs(np.fft.rfft(x_corr)*2./nsamp)**2,axis=0))
-    fft_y_corr = np.sqrt(np.mean(np.abs(np.fft.rfft(y_corr)*2./nsamp)**2,axis=0))
+    asd_x_raw = np.sqrt(np.mean(np.abs(np.fft.rfft(x_raw))**2*2./nsamp/fsamp,axis=0))
+    asd_y_raw = np.sqrt(np.mean(np.abs(np.fft.rfft(y_raw))**2*2./nsamp/fsamp,axis=0))
+    asd_x_corr = np.sqrt(np.mean(np.abs(np.fft.rfft(x_corr))**2*2./nsamp/fsamp,axis=0))
+    asd_y_corr = np.sqrt(np.mean(np.abs(np.fft.rfft(y_corr))**2*2./nsamp/fsamp,axis=0))
 
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
     fig,ax = plt.subplots()
-    ax.semilogy(freqs,fft_x_raw,label='Naive $x$',color=colors[0],alpha=0.5)
-    ax.semilogy(freqs,fft_y_raw,label='Naive $y$',color=colors[1],alpha=0.5)
-    ax.semilogy(freqs,fft_x_corr,label='Diag. $x$',color=colors[0],ls=':',lw=2)
-    ax.semilogy(freqs,fft_y_corr,label='Diag. $y$',color=colors[1],ls=':',lw=2)
+    ax.semilogy(freqs,asd_x_raw,label='Naive $x$',color=colors[0],alpha=0.5)
+    ax.semilogy(freqs,asd_y_raw,label='Naive $y$',color=colors[1],alpha=0.5)
+    ax.semilogy(freqs,asd_x_corr,label='Diag. $x$',color=colors[0],ls=':',lw=2)
+    ax.semilogy(freqs,asd_y_corr,label='Diag. $y$',color=colors[1],ls=':',lw=2)
     if (p_x is not None) and (p_y is not None):
         ax.semilogy(freqs,lor(freqs,*p_x),label='Peak fit $x$',color=colors[2])
         ax.semilogy(freqs,lor(freqs,*p_y),label='Peak fit $y$',color=colors[3])
@@ -271,18 +273,21 @@ def spectra(agg_dict,descrip=None,harms=[],which='roi',ylim=None):
         descrip = datetime.fromtimestamp(agg_dict['timestamp'][0]).strftime('%Y%m%d')
 
     freqs = agg_dict['freqs'][0]
+    nsamp = agg_dict['times'].shape[1]
+    fsamp = 2.*freqs[-1]
+    fft_to_asd = np.sqrt(nsamp/2./fsamp)
 
-    qpd_x_amps = np.abs(agg_dict['qpd_ffts_full'][:,0,:])
-    qpd_y_amps = np.abs(agg_dict['qpd_ffts_full'][:,1,:])
-    pspd_x_amps = np.abs(agg_dict['pspd_ffts_full'][:,0,:])
-    pspd_y_amps = np.abs(agg_dict['pspd_ffts_full'][:,1,:])
-    z_amps = np.abs(agg_dict['qpd_ffts_full'][:,2,:])
+    qpd_x_asds = np.abs(agg_dict['qpd_ffts_full'][:,0,:]*fft_to_asd)
+    qpd_y_asds = np.abs(agg_dict['qpd_ffts_full'][:,1,:]*fft_to_asd)
+    pspd_x_asds = np.abs(agg_dict['pspd_ffts_full'][:,0,:]*fft_to_asd)
+    pspd_y_asds = np.abs(agg_dict['pspd_ffts_full'][:,1,:]*fft_to_asd)
+    z_asds = np.abs(agg_dict['qpd_ffts_full'][:,2,:]*fft_to_asd)
 
-    qpd_x_amp = np.mean(qpd_x_amps,axis=0)
-    qpd_y_amp = np.mean(qpd_y_amps,axis=0)
-    pspd_x_amp = np.mean(pspd_x_amps,axis=0)
-    pspd_y_amp = np.mean(pspd_y_amps,axis=0)
-    z_amp = np.mean(z_amps,axis=0)
+    qpd_x_asd = np.sqrt(np.mean(qpd_x_asds**2,axis=0))
+    qpd_y_asd = np.sqrt(np.mean(qpd_y_asds**2,axis=0))
+    pspd_x_asd = np.sqrt(np.mean(pspd_x_asds**2,axis=0))
+    pspd_y_asd = np.sqrt(np.mean(pspd_y_asds**2,axis=0))
+    z_asd = np.sqrt(np.mean(z_asds**2,axis=0))
 
     fig,ax = plt.subplots()
     if len(harms):
@@ -291,34 +296,34 @@ def spectra(agg_dict,descrip=None,harms=[],which='roi',ylim=None):
     ax.set_xlabel('Frequency [Hz]')
     if which=='roi':
         if ylim is None:
-            ylim = [2e-18,2e-14]
-        ax.semilogy(freqs,qpd_x_amp,lw=1,alpha=0.65,label='QPD $x$')
-        ax.semilogy(freqs,qpd_y_amp,lw=1,alpha=0.65,label='QPD $y$')
-        ax.semilogy(freqs,pspd_x_amp,lw=1,alpha=0.65,label='PSPD $x$')
-        ax.semilogy(freqs,pspd_y_amp,lw=1,alpha=0.65,label='PSPD $y$')
-        ax.semilogy(freqs,z_amp,lw=1,alpha=0.65,label='$z$')
+            ylim = [1e-17,2e-14]
+        ax.semilogy(freqs,qpd_x_asd,lw=1,alpha=0.65,label='QPD $x$')
+        ax.semilogy(freqs,qpd_y_asd,lw=1,alpha=0.65,label='QPD $y$')
+        ax.semilogy(freqs,pspd_x_asd,lw=1,alpha=0.65,label='PSPD $x$')
+        ax.semilogy(freqs,pspd_y_asd,lw=1,alpha=0.65,label='PSPD $y$')
+        ax.semilogy(freqs,z_asd,lw=1,alpha=0.65,label='$z$')
         ax.set_xlim([0.1,50])
         ax.set_ylim(ylim)
         ax.set_title('Calibrated spectra in ROI for '+descrip)
     elif which=='full':
         if ylim is None:
-            ylim = [2e-19,2e-14]
-        ax.loglog(freqs,qpd_x_amp,lw=1,alpha=0.65,label='QPD $x$')
-        ax.loglog(freqs,qpd_y_amp,lw=1,alpha=0.65,label='QPD $y$')
-        ax.loglog(freqs,pspd_x_amp,lw=1,alpha=0.65,label='PSPD $x$')
-        ax.loglog(freqs,pspd_y_amp,lw=1,alpha=0.65,label='PSPD $y$')
-        ax.loglog(freqs,z_amp,lw=1,alpha=0.65,label='$z$')
+            ylim = [1e-18,2e-14]
+        ax.loglog(freqs,qpd_x_asd,lw=1,alpha=0.65,label='QPD $x$')
+        ax.loglog(freqs,qpd_y_asd,lw=1,alpha=0.65,label='QPD $y$')
+        ax.loglog(freqs,pspd_x_asd,lw=1,alpha=0.65,label='PSPD $x$')
+        ax.loglog(freqs,pspd_y_asd,lw=1,alpha=0.65,label='PSPD $y$')
+        ax.loglog(freqs,z_asd,lw=1,alpha=0.65,label='$z$')
         ax.set_xlim([0.1,2500])
         ax.set_ylim(ylim)
         ax.set_title('Full calibrated spectra for '+descrip)
     elif which=='rayleigh':
         if ylim is None:
             ylim = [0,2]
-        ax.plot(freqs,rayleigh(qpd_x_amps**2),lw=1,alpha=0.65,label='QPD $x$')
-        ax.plot(freqs,rayleigh(qpd_y_amps**2),lw=1,alpha=0.65,label='QPD $y$')
-        ax.plot(freqs,rayleigh(pspd_x_amps**2),lw=1,alpha=0.65,label='PSPD $x$')
-        ax.plot(freqs,rayleigh(pspd_y_amps**2),lw=1,alpha=0.65,label='PSPD $y$')
-        ax.plot(freqs,rayleigh(z_amps**2),lw=1,alpha=0.65,label='$z$')
+        ax.plot(freqs,rayleigh(qpd_x_asds**2),lw=1,alpha=0.65,label='QPD $x$')
+        ax.plot(freqs,rayleigh(qpd_y_asds**2),lw=1,alpha=0.65,label='QPD $y$')
+        ax.plot(freqs,rayleigh(pspd_x_asds**2),lw=1,alpha=0.65,label='PSPD $x$')
+        ax.plot(freqs,rayleigh(pspd_y_asds**2),lw=1,alpha=0.65,label='PSPD $y$')
+        ax.plot(freqs,rayleigh(z_asds**2),lw=1,alpha=0.65,label='$z$')
         ax.set_ylabel('Rayleigh statistic [1/$\sqrt{\mathrm{Hz}}$]')
         ax.set_xlabel('Frequency [Hz]')
         ax.set_xlim([0.1,50])
@@ -328,8 +333,8 @@ def spectra(agg_dict,descrip=None,harms=[],which='roi',ylim=None):
     ax.legend(ncol=3)
 
     # for some reason memory is not released and in subsequent function calls this can cause errors
-    del qpd_x_amps,qpd_y_amps,pspd_x_amps,pspd_y_amps,z_amps,\
-        qpd_x_amp,qpd_y_amp,pspd_x_amp,z_amp,freqs
+    del qpd_x_asds,qpd_y_asds,pspd_x_asds,pspd_y_asds,z_asds,\
+        qpd_x_asd,qpd_y_asd,pspd_x_asd,pspd_y_asd,z_asd,freqs
 
     return fig,ax
 
@@ -343,7 +348,10 @@ def spectrogram(agg_dict,descrip=None,sensor='qpd',axis_ind=0,which='roi',\
         descrip = datetime.fromtimestamp(agg_dict['timestamp'][0]).strftime('%Y%m%d')
 
     freqs = agg_dict['freqs'][0]
-    amps = np.abs(agg_dict[sensor+'_ffts_full'][:,axis_ind,:])
+    nsamp = agg_dict['times'].shape[1]
+    fsamp = 2.*freqs[-1]
+    fft_to_asd = np.sqrt(nsamp/2./fsamp)
+    asds = np.abs(agg_dict[sensor+'_ffts_full'][:,axis_ind,:])*fft_to_asd
 
     times = agg_dict['times']
     av_times = np.mean(times,axis=1)
@@ -354,7 +362,7 @@ def spectrogram(agg_dict,descrip=None,sensor='qpd',axis_ind=0,which='roi',\
     t_bins = int(round(t_bin_width/delta_t))
     num_t_bins = int(len(av_times)/t_bins)
     spec_ray = np.zeros((num_t_bins,len(freqs)))
-    spec_amp = np.zeros((num_t_bins,len(freqs)))
+    spec_asd = np.zeros((num_t_bins,len(freqs)))
     plot_times = np.zeros(num_t_bins)
 
     fig,ax = plt.subplots()
@@ -363,9 +371,9 @@ def spectrogram(agg_dict,descrip=None,sensor='qpd',axis_ind=0,which='roi',\
             vmin = 2e-18
             vmax = 2e-14
         for i in range(spec_ray.shape[0]):
-            spec_amp[i,:] = np.mean(amps[i*t_bins:(i+1)*t_bins,:],axis=0)
+            spec_asd[i,:] = np.sqrt(np.mean(asds[i*t_bins:(i+1)*t_bins,:]**2,axis=0))
             plot_times[i] = np.mean(hours[i*t_bins:(i+1)*t_bins])
-        pcm = ax.pcolormesh(plot_times,freqs,spec_amp.T,norm=LogNorm(vmin=vmin,vmax=vmax),cmap='magma')
+        pcm = ax.pcolormesh(plot_times,freqs,spec_asd.T,norm=LogNorm(vmin=vmin,vmax=vmax),cmap='magma')
         ax.set_ylabel('Frequency [Hz]')
         ax.set_ylim([0.1,50])
         ax.set_xlabel('Time since '+start_date+' [hours]')
@@ -377,9 +385,9 @@ def spectrogram(agg_dict,descrip=None,sensor='qpd',axis_ind=0,which='roi',\
             vmin = 2e-19
             vmax = 2e-14
         for i in range(spec_ray.shape[0]):
-            spec_amp[i,:] = np.mean(amps[i*t_bins:(i+1)*t_bins,:],axis=0)
+            spec_asd[i,:] = np.sqrt(np.mean(asds[i*t_bins:(i+1)*t_bins,:]**2,axis=0))
             plot_times[i] = np.mean(hours[i*t_bins:(i+1)*t_bins])
-        pcm = ax.pcolormesh(plot_times,freqs,spec_amp.T,norm=LogNorm(vmin=vmin,vmax=vmax),cmap='magma')
+        pcm = ax.pcolormesh(plot_times,freqs,spec_asd.T,norm=LogNorm(vmin=vmin,vmax=vmax),cmap='magma')
         ax.set_ylabel('Frequency [Hz]')
         ax.set_ylim([0.1,2500])
         ax.set_xlabel('Time since '+start_date+' [hours]')
@@ -392,7 +400,7 @@ def spectrogram(agg_dict,descrip=None,sensor='qpd',axis_ind=0,which='roi',\
             vmin = 0
             vmax = 2
         for i in range(spec_ray.shape[0]):
-            spec_ray[i,:] = rayleigh(amps[i*t_bins:(i+1)*t_bins,:]**2)
+            spec_ray[i,:] = rayleigh(asds[i*t_bins:(i+1)*t_bins,:]**2)
             plot_times[i] = np.mean(hours[i*t_bins:(i+1)*t_bins])
         pcm = ax.pcolormesh(plot_times,freqs,spec_ray.T,vmin=vmin,vmax=vmax,cmap='coolwarm')
         ax.set_ylabel('Frequency [Hz]')
@@ -403,7 +411,7 @@ def spectrogram(agg_dict,descrip=None,sensor='qpd',axis_ind=0,which='roi',\
         cbar.set_label('Rayleigh statistic [1/$\sqrt{\mathrm{Hz}}$]',rotation=270,labelpad=15)
 
     # for some reason memory is not released and in subsequent function calls this can cause errors
-    del freqs,amps,times,av_times,start_date,hours
+    del freqs,asds,times,av_times,start_date,hours
 
     return fig,ax
 
@@ -422,31 +430,34 @@ def time_evolution(agg_dict,descrip=None,sensor='qpd',axis_ind=0,\
     start_date = datetime.fromtimestamp(av_times[0]*1e-9).strftime('%b %d, %H:%M:%S')
     hours = (av_times-av_times[0])*1e-9/3600.
     freqs = agg_dict['freqs'][0]
+    nsamp = agg_dict['times'].shape[1]
+    fsamp = 2.*freqs[-1]
+    fft_to_asd = np.sqrt(nsamp/2./fsamp)
     good_freqs = freqs[agg_dict['good_inds'][0]]
     axes = ['x','y','z']
     colors = plt.get_cmap('plasma',len(good_freqs)+1)
 
     # get amplitude and phase for the sensor and axis
-    amps = np.abs(agg_dict[sensor+'_ffts'][:,axis_ind,:])
+    asds = np.abs(agg_dict[sensor+'_ffts'][:,axis_ind,:])*fft_to_asd
     phases = np.angle(agg_dict[sensor+'_ffts'][:,axis_ind,:])*180./np.pi
 
     # average by time
     delta_t = (av_times[1]-av_times[0])*1e-9
     t_bins = int(round(t_bin_width/delta_t))
     num_t_bins = int(len(av_times)/t_bins)
-    amp_t = np.zeros((num_t_bins,amps.shape[1]))
-    phase_t = np.zeros((num_t_bins,amps.shape[1]))
+    asd_t = np.zeros((num_t_bins,asds.shape[1]))
+    phase_t = np.zeros((num_t_bins,asds.shape[1]))
     plot_times = np.zeros(num_t_bins)
 
     for i in range(num_t_bins):
-        amp_t[i,:] = np.mean(amps[i*t_bins:(i+1)*t_bins,:],axis=0)
+        asd_t[i,:] = np.sqrt(np.mean(asds[i*t_bins:(i+1)*t_bins,:]**2,axis=0))
         phase_t[i,:] = np.mean(phases[i*t_bins:(i+1)*t_bins,:],axis=0)
         plot_times[i] = np.mean(hours[i*t_bins:(i+1)*t_bins])
 
     # plot the results
     fig,ax = plt.subplots(2,1,figsize=(8,8),sharex=True)
     for i in range(len(good_freqs)):
-        ax[0].semilogy(plot_times,amp_t[:,i],ls='none',marker='o',ms=2,alpha=0.65,\
+        ax[0].semilogy(plot_times,asd_t[:,i],ls='none',marker='o',ms=2,alpha=0.65,\
                     label='{:.1f} Hz'.format(good_freqs[i]),color=colors(i))
         ax[1].plot(plot_times,phase_t[:,i],ls='none',marker='o',ms=2,alpha=0.65,\
                 label='{:.1f} Hz'.format(good_freqs[i]),color=colors(i))
@@ -465,8 +476,8 @@ def time_evolution(agg_dict,descrip=None,sensor='qpd',axis_ind=0,\
     ax[1].grid(which='both')
 
     # for some reason memory is not released and in subsequent function calls this can cause errors
-    del freqs,amps,phases,times,av_times,start_date,hours,\
-        plot_times,amp_t,phase_t,num_t_bins,t_bins,delta_t
+    del freqs,asds,phases,times,av_times,start_date,hours,\
+        plot_times,asd_t,phase_t,num_t_bins,t_bins,delta_t
 
     return fig,ax
 
@@ -484,7 +495,7 @@ def position_drift(agg_dict,descrip=None,t_bin_width=600):
     pt = agg_dict['mean_p_trans']
     bh = agg_dict['bead_height']
     cx = agg_dict['mean_cant_pos'][:,0]
-    cz = agg_dict['mean_cant_pos'][:,1]
+    cz = agg_dict['mean_cant_pos'][:,2]
     x0 = cx[0]
     z0 = cz[0]
     cx = cx - x0